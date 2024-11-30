c370b89208da19bda233610a70e3cac90dbba2e4
Kwe <Kweayon@gmail.com>
2024-11-30 13:46:16 -0600

    task: separated css from TextFormField

--- src/features/shared/components/text_form_field.tsx
+++ src/features/shared/components/text_form_field.tsx
@@ -1,5 +1,11 @@
 import showPassword from "../../../assets/images/icon-show-password.svg";
+import hidePassword from "../../../assets/images/icon-hide-password.svg";
+
 import { MouseEventHandler } from "react";
+import "./css/text_form_field.css";
+
+//!! TODO: conver px to rem
+
 const TextFormField = ({
   name,
   label,
@@ -17,33 +23,27 @@ const TextFormField = ({
     <>
       <label htmlFor={name}>{label}</label>
       <div style={{ height: "4px" }} />
-      <input
-        id={name}
-        title={name}
-        name={name}
-        type={type}
-        style={{
-          border: "1px solid #98908B",
-          height: "42px",
-          borderRadius: "8px",
-          paddingLeft: "12px",
-        }}
-      />
-      {showPasswordIcon ? (
-        <img
-          src={showPassword}
-          alt="hello"
-          style={{
-            width: "16px",
-            position: "relative",
-            bottom: "25px",
-            left: "92.5%",
-          }}
-          onClick={onIconTap}
+      <div className="textFormFieldContainer">
+        <input
+          id={name}
+          title={name}
+          name={name}
+          type={type}
+          className="textFormField"
         />
-      ) : (
-        <></>
-      )}
+        {showPasswordIcon ? (
+          <img
+            src={type === "text" ? hidePassword : showPassword}
+            alt="hello"
+            style={{
+              paddingRight: "12px",
+            }}
+            onClick={onIconTap}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
     </>
   );
 };
