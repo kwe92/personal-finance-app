04810010b37c93e429de092d9f766bcc29bbd6a1
Kwe <Kweayon@gmail.com>
2024-12-02 11:12:55 -0600

    feat: implemented auth app bar

---
+++ src/features/auth/shared/components/auth_app_bar.tsx
@@ -0,0 +1,12 @@
+import logo from "../../../../assets/images/logo-large.svg";
+import "../css/auth_app_bar.css";
+
+const AuthAppBar = (): JSX.Element => {
+  return (
+    <div id="auth-app-bar">
+      <img src={logo}></img>
+    </div>
+  );
+};
+
+export default AuthAppBar;
