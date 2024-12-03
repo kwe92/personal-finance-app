6dfab03cd0a86574ea2ea810eb1dc9cc9d36d864
Kwe <Kweayon@gmail.com>
2024-12-03 13:31:48 -0600

    task: implemented home view

---
+++ src/features/home/home_view.tsx
@@ -0,0 +1,12 @@
+import SideNavBar from "./components/side_nav_bar";
+
+const Home = (): JSX.Element => {
+  return (
+    // <> </>
+    <div style={{ display: "flex", width: "100%", height: "100%" }}>
+      <SideNavBar />
+    </div>
+  );
+};
+
+export default Home;
