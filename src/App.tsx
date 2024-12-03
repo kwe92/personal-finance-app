af47dd7575b968359dab7d69f3841d225e8abfe7
Kwe <Kweayon@gmail.com>
2024-12-03 16:45:57 -0600

    task: updated routing
    	- added use effect hook to manage state of showing the SideNavBar component and AuthImage component
    
    	- added auth and home routes and sub-routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,68 @@
 import "./app/theme/css/App.css";
-import React from "react";
-import { Route, Routes, useLocation } from "react-router";
+import { useEffect, useState } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router";
 import { AnimatePresence } from "framer-motion";
 import LoginView from "./features/auth/login/ui/login_view";
 import SignUpView from "./features/auth/signUp/ui/sign_up_view";
 import Transitions from "./app/theme/transitions";
-import HomeView from "./features/home/home_view";
 import OverviewView from "./features/overview/overview_view";
 import TransactionsView from "./features/transactions/transactions_view";
 import SideNavBar from "./features/home/components/side_nav_bar";
+import AuthImage from "./features/auth/shared/components/auth_image";
 
 function App() {
   const location = useLocation();
+
+  const [showSideNavBar, setShowSideNavBar] = useState(false);
+
+  console.log(`location path name: ${location.pathname}`);
+
+  useEffect(() => {
+    if (location.pathname.includes("home")) {
+      setShowSideNavBar(true);
+    }
+  });
+
   return (
     <div className="App">
-      {/* <SideNavBar /> */}
+      {!showSideNavBar && <AuthImage />}
+
+      {showSideNavBar && <SideNavBar />}
 
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
-          {/* <Route path="/" element={Transitions.fade(<LoginView />)} />
-          <Route path="/signUp" element={Transitions.fade(<SignUpView />)} /> */}
+          <Route
+            path="/"
+            element={
+              false // TODO: should useContext and some sort of AuthProvider to track is logged in state
+                ? Transitions.fade(<OverviewView />)
+                : Transitions.fade(<Navigate to="/auth/login" />)
+            }
+          />
+
+          <Route
+            path="/auth/*"
+            element={Transitions.fade(<Navigate to={"/auth/login"} />)}
+          />
+
+          <Route path="/auth/login" element={Transitions.fade(<LoginView />)} />
+
+          <Route
+            path="/auth/signUp"
+            element={Transitions.fade(<SignUpView />)}
+          />
 
           {/* TODO: change path name to home  */}
-          <Route path="/" element={Transitions.fade(<HomeView />)} />
           <Route
-            path="/Overview"
+            path="/home/*"
+            element={Transitions.fade(<Navigate to={"/home/Overview"} />)}
+          />
+          <Route
+            path="/home/Overview"
             element={Transitions.fade(<OverviewView />)}
           />
           <Route
-            path="/Transactions"
+            path="/home/Transactions"
             element={Transitions.fade(<TransactionsView />)}
           />
         </Routes>
