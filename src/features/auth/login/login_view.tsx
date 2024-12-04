1467ebb6373f00477366e7442e4dd35f7e68a144
Kwe <Kweayon@gmail.com>
2024-12-04 12:44:24 -0600

    refact: removed unused folders

--- src/features/auth/login/login_view.tsx
+++ src/features/auth/login/login_view.tsx
@@ -1,12 +1,11 @@
 import AuthAppBar from "../../shared/components/auth_app_bar";
-import AuthForm from "../../shared/components/auth_form";
-import AuthImage from "../../shared/components/auth_image";
+import AuthForm from "../shared/components/auth_form";
+import AuthImage from "../shared/components/auth_image";
 
 const LoginView = (): JSX.Element => {
   return (
     <>
       <AuthForm />
-      <AuthAppBar />
     </>
   );
 };
--- src/features/auth/signUp/sign_up_view.tsx
+++ src/features/auth/signUp/sign_up_view.tsx
@@ -1,12 +1,11 @@
-import AuthImage from "../../shared/components/auth_image";
-import AuthForm from "../../shared/components/auth_form";
+import AuthImage from "../shared/components/auth_image";
+import AuthForm from "../shared/components/auth_form";
 import AuthAppBar from "../../shared/components/auth_app_bar";
 
 const SignUpView = (): JSX.Element => {
   return (
     <>
       <AuthForm isLogin={false} />
-      <AuthAppBar />
     </>
   );
 };
