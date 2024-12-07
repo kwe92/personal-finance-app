95d41295695b94ff75dead7dbb54d1d105f33153
Kwe <Kweayon@gmail.com>
2024-12-07 11:27:42 -0600

    refact: moved overview view inline css to css file

--- src/features/overview/overview_view.tsx
+++ src/features/overview/overview_view.tsx
@@ -1,3 +1,4 @@
+import "./overview_view.css";
 import * as gaps from "../../app/constants/reusable";
 import OverviewPotsTransactionSection from "./components/overview_pots_transaction_section";
 import OverviewListTileRow from "./components/overview_list_tile_row";
@@ -5,15 +6,7 @@ import OverviewSecondSection from "./components/overview_second_section";
 
 const OverviewView = (): JSX.Element => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-        height: "100%",
-        padding: "32px 32px 32px 32px",
-      }}
-    >
+    <div className="overview-view-main">
       <h1 style={{ color: "#201F24" }}>Overview</h1>
 
       <gaps.GapH32 />
@@ -23,8 +16,7 @@ const OverviewView = (): JSX.Element => {
       {/* lower section */}
 
       <gaps.GapH32 />
-
-      <div style={{ width: "100%", height: "100%", display: "flex" }}>
+      <div className="overview-view-secondary">
         <OverviewPotsTransactionSection />
 
         <gaps.GapW24 />
