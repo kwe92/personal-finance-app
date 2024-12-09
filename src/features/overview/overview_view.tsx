e70054805c8bc3f618f03d076ec8b4ac888d5f9c
Kwe <Kweayon@gmail.com>
2024-12-09 12:42:37 -0600

    task: removed static gaps and ensured responsiveness

--- src/features/overview/overview_view.tsx
+++ src/features/overview/overview_view.tsx
@@ -1,27 +1,20 @@
 import "./overview_view.css";
-import * as gaps from "../../app/constants/reusable";
 import OverviewPotsTransactionSection from "./components/overview_pots_transaction_section";
 import OverviewListTileRow from "./components/overview_list_tile_row";
-import OverviewSecondSection from "./components/overview_second_section";
+import OverviewBudgetBillsSection from "./components/overview-budget-bills-section";
+import { GapH64 } from "../../app/constants/reusable";
 
 const OverviewView = (): JSX.Element => {
   return (
     <div className="overview-view-main">
       <h1 style={{ color: "#201F24" }}>Overview</h1>
 
-      <gaps.GapH32 />
-
       <OverviewListTileRow />
 
-      {/* lower section */}
-
-      <gaps.GapH32 />
       <div className="overview-view-secondary">
         <OverviewPotsTransactionSection />
 
-        <gaps.GapW24 />
-
-        <OverviewSecondSection />
+        <OverviewBudgetBillsSection />
       </div>
     </div>
   );
