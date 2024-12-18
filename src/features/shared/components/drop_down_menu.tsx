35637bc553fc6781e849e2975173761e3f3f880b
Kwe <Kweayon@gmail.com>
2024-12-18 09:42:57 -0600

    refact: ensured that the drop down lists now filter transactions properly

--- src/features/shared/components/drop_down_menu.tsx
+++ src/features/shared/components/drop_down_menu.tsx
@@ -1,22 +1,53 @@
 import "./css/drop_down_menu.css";
 import caretDown from "../../../assets/images/icon-caret-down.svg";
+import { Divider } from "./divider";
 
 export const DropDownMenu = ({
+  id,
   title,
   label,
-  onTap,
+  content,
+  onMenuTap,
+  onItemTap,
 }: {
+  id?: string;
   title: string;
-  label: string; // TODO: label needs to change dynamically based on user selection
-  onTap: React.MouseEventHandler<HTMLDivElement>;
+  label: string;
+  content: string[];
+  onMenuTap?: React.MouseEventHandler;
+  onItemTap?: Function;
 }): JSX.Element => {
+  const dropDownMenuItems = content.map((menuItem, i) => {
+    return (
+      <>
+        <li
+          key={i}
+          onClick={() => {
+            onItemTap?.(menuItem);
+          }}
+        >
+          {menuItem}
+        </li>
+        {content.length - 1 !== i ? <Divider /> : <></>}
+      </>
+    );
+  });
+
   return (
-    <div className="drop-down-menu-container" onClick={onTap}>
-      <p style={{ color: "#696868" }}>{title}</p>
-      <div className="drop-down-menu-content">
-        <p>{label}</p>
-        <img src={caretDown} alt="caret down" />
+    <div
+      id={id}
+      className="dropdown"
+      style={{ position: "relative" }}
+      onClick={onMenuTap}
+    >
+      <div className="drop-down-menu-container">
+        <p style={{ color: "#696868" }}>{title}</p>
+        <div className="drop-down-menu-selector">
+          <p>{label}</p>
+          <img src={caretDown} alt="caret down" />
+        </div>
       </div>
+      <ul className="drop-down-menu-content">{dropDownMenuItems}</ul>
     </div>
   );
 };
