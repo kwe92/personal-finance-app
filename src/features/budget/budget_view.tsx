0c94ec9118a08a2d348ed9f17eb620ed28dbd35d
Kwe <Kweayon@gmail.com>
2024-12-26 10:07:31 -0600

    implemented budget view model to abstract away budget view business logic

--- src/features/budget/budget_view.tsx
+++ src/features/budget/budget_view.tsx
@@ -1,29 +1,38 @@
 import "./budget_view.css";
-import { Divider } from "../shared/components/divider";
-
 import { AddNewButton } from "../shared/components/add_new_button";
 import { BudgetSummary } from "./components/budget_summary";
-import * as gaps from "../../app/constants/reusable";
-
 import { BudgetCard } from "./components/budget_card";
+import { ModalWrapper } from "../shared/components/modal_wrapper";
+import { BudgetViewModel } from "./budget_view_model";
+import { AddNewBudgetCard } from "./components/add_new_budget_card";
 
 // TODO: remove hard coded values
 
 export const BudgetView = (): JSX.Element => {
+  const viewModel = BudgetViewModel.getInstance();
+
   return (
-    <div className="budget-view-main">
-      <div className="budget-header-button-section">
-        <h1 style={{ color: "#201F24" }}>Budgets</h1>
+    <>
+      <div className="budget-view-main">
+        <div className="budget-header-button-section">
+          <h1 style={{ color: "#201F24" }}>Budgets</h1>
 
-        <AddNewButton label="Budget" />
+          <AddNewButton
+            label="Budget"
+            onTap={viewModel.toogleAddNewBudgetModal}
+          />
+        </div>
+        <BudgetSummary />
+
+        <BudgetCard index={0} />
+
+        <BudgetCard index={1} />
+
+        <BudgetCard index={2} />
       </div>
-      <BudgetSummary />
-
-      <BudgetCard index={0} />
-
-      <BudgetCard index={1} />
-
-      <BudgetCard index={2} />
-    </div>
+      <ModalWrapper id="add-new-budget-modal">
+        <AddNewBudgetCard onTap={viewModel.toogleAddNewBudgetModal} />
+      </ModalWrapper>
+    </>
   );
 };
