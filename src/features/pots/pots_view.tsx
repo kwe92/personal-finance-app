e3b55c67a7fbc873c0d870ed05961040a9b7180c
Kwe <Kweayon@gmail.com>
2025-01-07 20:40:59 -0600

    task: implemented pots view css file

--- src/features/pots/pots_view.tsx
+++ src/features/pots/pots_view.tsx
@@ -1,9 +1,10 @@
 import "../shared/css/view_container.css";
+import "./pots_view.css";
 import { AddNewButton } from "../shared/components/add_new_button";
+import { PotCard } from "./components/pot_card";
 
 export const PotsView = (): JSX.Element => {
   return (
-    // TODO: move shared CSS to shared folder: budget-view-main, budget-header-button-section
     <div className="view-container">
       <div className="budget-header-button-section">
         <h1 style={{ color: "#201F24" }}>Pots</h1>
@@ -14,6 +15,12 @@ export const PotsView = (): JSX.Element => {
             // TODO: implement, see Budget View for details
           }}
         />
+      </div>
+
+      <div className="pots-view-content">
+        <PotCard totalSaved={200} target={2000} color={"blue"} />
+        <PotCard totalSaved={200} target={2000} color={"blue"} />
+        <PotCard totalSaved={200} target={2000} color={"blue"} />
       </div>
     </div>
   );
