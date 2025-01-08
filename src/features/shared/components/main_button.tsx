90445ac35c0152ef8a992b83ed6d5ca070cea926
Kwe <Kweayon@gmail.com>
2025-01-07 20:43:41 -0600

    task: added style prop to MainButton component

--- src/features/shared/components/main_button.tsx
+++ src/features/shared/components/main_button.tsx
@@ -3,10 +3,16 @@ import "./css/main_button.css";
 const MainButton = (props: {
   children?: React.ReactNode;
   type?: "submit" | "reset" | "button" | undefined;
+  style?: React.CSSProperties;
   onTap?: React.MouseEventHandler<HTMLButtonElement>;
 }): JSX.Element => {
   return (
-    <button className="main-button" type={props.type} onClick={props.onTap}>
+    <button
+      className="main-button"
+      style={props.style}
+      type={props.type}
+      onClick={props.onTap}
+    >
       {props.children}
     </button>
   );
