53ca438a032f00185e368a5d771af46ff94cab4c
Kwe <Kweayon@gmail.com>
2025-01-07 20:42:00 -0600

    feat: initial pot card implementation

---
+++ src/features/pots/components/pot_card.tsx
@@ -0,0 +1,115 @@
+import "./css/pot_card.css";
+
+import { CardHeader } from "../../shared/components/card_header";
+import MainButton from "../../shared/components/main_button";
+
+// TODO: props should eventually be a single Model / DTO
+export const PotCard = ({
+  totalSaved,
+  target,
+  color,
+}: {
+  totalSaved: number;
+  target: number;
+  color: string;
+}): JSX.Element => {
+  const percentSaved = pctTotal(totalSaved, target);
+
+  const buttonStyle = {
+    backgroundColor: "#F8F4F0",
+    border: "none",
+    color: "black",
+  };
+  return (
+    <div className="pot-card-main">
+      {/* card header */}
+
+      <CardHeader
+        color={color}
+        name={"Savings"}
+        dropdownText="Pot"
+        dropdownClassName="pot-card-dropdown"
+        onIconTap={() => {
+          // TODO: implement
+        }}
+        handleEditItem={() => {
+          // TODO: implement
+        }}
+        handleDeleteItem={() => {
+          // TODO: implement
+        }}
+      />
+
+      <div className="pot-card-total-saved">
+        <p>Total Saved</p>
+        <p>${totalSaved.toFixed(2)}</p>
+      </div>
+
+      {/* progress bar / percent saved */}
+      <div className="pot-card-progress">
+        <PotProgressBar totalSaved={totalSaved} target={target} color="blue" />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <p>{percentSaved.toFixed(2)}%</p>
+
+          <p>Target of ${target.toFixed(2)}</p>
+        </div>
+      </div>
+
+      {/* buttons */}
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          alignItems: "center",
+          gap: "16px",
+        }}
+      >
+        <MainButton style={buttonStyle}>+ Add Money</MainButton>
+        <MainButton style={buttonStyle}>Withdraw</MainButton>
+      </div>
+    </div>
+  );
+};
+
+const PotProgressBar = ({
+  totalSaved,
+  target,
+  color,
+}: {
+  totalSaved: number;
+  target: number;
+  color: string;
+}): JSX.Element => {
+  const percentSaved = pctTotal(totalSaved, target);
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "12px",
+        backgroundColor: "#F8F4F0",
+        borderRadius: "8px",
+      }}
+    >
+      <div
+        style={{
+          width: `${percentSaved}%`,
+          height: "inherit",
+          backgroundColor: percentSaved < 100 ? color : "lightgreen",
+          borderRadius: "8px",
+        }}
+      />
+    </div>
+  );
+};
+
+function pctTotal(amount: number, total: number): number {
+  const percentOfTotal = (amount / total) * 100;
+
+  return percentOfTotal;
+}
