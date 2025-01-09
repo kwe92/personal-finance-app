f155008d67cc2e49c645dcf9781c9543b738621f
Kwe <Kweayon@gmail.com>
2025-01-09 11:19:50 -0600

    task: moved constant data to constant folder

---
+++ src/app/constants/constants.tsx
@@ -0,0 +1,62 @@
+export const colorTagData = [
+  new Map([
+    ["name", "Green"],
+    ["theme", "#277C78"],
+  ]),
+  new Map([
+    ["name", "Yellow"],
+    ["theme", "#F2CDAC"],
+  ]),
+  new Map([
+    ["name", "Cyan"],
+    ["theme", "#82C9D7"],
+  ]),
+  new Map([
+    ["name", "Navy"],
+    ["theme", "#201F24"],
+  ]),
+  new Map([
+    ["name", "Red"],
+    ["theme", "#C94736"],
+  ]),
+  new Map([
+    ["name", "Purple"],
+    ["theme", "#826CB0"],
+  ]),
+  new Map([
+    ["name", "Turquoise"],
+    ["theme", "#597C7C"],
+  ]),
+  new Map([
+    ["name", "Brown"],
+    ["theme", "#93674F"],
+  ]),
+  new Map([
+    ["name", "Magenta"],
+    ["theme", "#934F6F"],
+  ]),
+  new Map([
+    ["name", "Blue"],
+    ["theme", "#3F82B2"],
+  ]),
+  new Map([
+    ["name", "Navy Grey"],
+    ["theme", "#97A0AC"],
+  ]),
+  new Map([
+    ["name", "Army Green"],
+    ["theme", "#7F9161"],
+  ]),
+  new Map([
+    ["name", "Pink"],
+    ["theme", "#fb928e"],
+  ]),
+  new Map([
+    ["name", "Gold"],
+    ["theme", "#CAB361"],
+  ]),
+  new Map([
+    ["name", "Orange"],
+    ["theme", "#BE6C49"],
+  ]),
+];
