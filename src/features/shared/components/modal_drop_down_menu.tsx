dc35cbea03c472bf4c925eb5720661830b2fa722
Kwe <Kweayon@gmail.com>
2025-01-09 11:24:02 -0600

    feat: implemented shared modal

---
+++ src/features/shared/components/modal_drop_down_menu.tsx
@@ -0,0 +1,47 @@
+import "./css/modal_drop_down_menu.css";
+import caretDown from "../../../assets/images/icon-caret-down.svg";
+
+export const ModalDropDownMenu = ({
+  label,
+  content,
+  initialValue,
+  isColorTag = false,
+  tagColor,
+  toggleMenu,
+}: {
+  label: string;
+  content: JSX.Element[];
+  initialValue: string;
+  isColorTag?: boolean;
+  tagColor?: string;
+  toggleMenu: React.MouseEventHandler;
+}): JSX.Element => {
+  return (
+    <div className="modal-drop-down-menu" onClick={toggleMenu}>
+      <label>{label}</label>
+      {/* dropdown selector*/}
+      <div className="modal-drop-down-menu-selector">
+        {isColorTag ? (
+          <div className="modal-drop-down-menu-colored-tag-section">
+            {/* circle */}
+            <div
+              style={{
+                width: "16px",
+                height: "16px",
+                borderRadius: "8px",
+                backgroundColor: tagColor,
+              }}
+            />
+            <p>{initialValue}</p>
+          </div>
+        ) : (
+          <p>{initialValue}</p>
+        )}
+
+        <img src={caretDown} alt="caret down" />
+      </div>
+      {/* dropdown content*/}
+      <div className="modal-drop-down-menu-content">{content}</div>
+    </div>
+  );
+};
