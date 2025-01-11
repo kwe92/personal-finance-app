d45a178adf109a8de0fa3cae7ae9b98e31596584
Kwe <Kweayon@gmail.com>
2025-01-11 15:00:19 -0600

    refact: ensured modal wrapper only needed one id

--- src/features/shared/components/modal_wrapper.tsx
+++ src/features/shared/components/modal_wrapper.tsx
@@ -2,16 +2,14 @@ import "./css/modal_wrapper.css";
 
 export const ModalWrapper = ({
   id,
-  contentId,
   children,
 }: {
   id: string;
-  contentId: string;
   children?: React.ReactNode;
 }): JSX.Element => {
   return (
     <div id={id} className="modal-background">
-      <div id={contentId}>{children}</div>
+      <div id={`${id}-content`}>{children}</div>
     </div>
   );
 };
