90d22be84c68481746fff741caec64d00ecd169d
Kwe <Kweayon@gmail.com>
2025-01-11 14:59:35 -0600

    feat: implemented delete modal to be shared for both pots and budgeet

--- src/features/shared/components/delete_modal.tsx
+++ src/features/shared/components/delete_modal.tsx
@@ -1,21 +1,58 @@
+import { ToastService } from "../services/toast_service";
 import { CloseModalButton } from "./close_modal_button";
+import MainButton from "./main_button";
+import TextButton from "./text_button";
 
 export const DeleteModal = ({
-  itemToDelete,
+  modalId,
+  isPot,
   title,
   handleItemDeletion,
 }: {
-  itemToDelete: PotData | BudgetData;
+  modalId: string;
+  isPot: boolean;
   title: string;
   handleItemDeletion: Function;
 }): JSX.Element => {
+  const toastService = ToastService.getInstance();
+
   return (
     <div className="base-modal">
       <div className="base-modal-header">
         <p>{`Delete '${title}'?`}</p>
-
-        <CloseModalButton onTap={() => {}} />
+        <CloseModalButton onTap={closeModal} />
       </div>
+      <p style={{ color: "#696868", fontSize: "14px" }}>
+        Are you sure you want to delete this{" "}
+        <span>{isPot ? "pot" : "budget"}</span>? This action cannot be reversed,
+        and all the data inside it will be removed forever.
+      </p>
+
+      <MainButton
+        style={{ backgroundColor: "#C94736", border: "none" }}
+        onTap={() => {
+          handleItemDeletion();
+          closeModal();
+        }}
+      >
+        Yes, Confirm Deletion
+      </MainButton>
+
+      <TextButton
+        onTap={closeModal}
+        style={{
+          textDecoration: "none",
+          color: "#696868",
+          fontSize: "14px",
+          fontWeight: "normal",
+        }}
+      >
+        No, Go Back
+      </TextButton>
     </div>
   );
+
+  function closeModal() {
+    toastService.closeModal(modalId);
+  }
 };
