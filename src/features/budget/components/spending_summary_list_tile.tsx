336df4b74c2a043a65b78d81360709594a399c6d
Kwe <Kweayon@gmail.com>
2025-01-21 18:09:41 -0600

    refact: removed inline styling and used BudgetViewModel to abstract away duplicated business logic

--- src/features/budget/components/spending_summary_list_tile.tsx
+++ src/features/budget/components/spending_summary_list_tile.tsx
@@ -1,5 +1,6 @@
 import { useTransactionData } from "../../shared/context/transaction_context";
 import { sortByDate } from "../../shared/utility/functions";
+import { BudgetViewModel } from "../budget_view_model";
 import "./css/spending_summary_list_tile.css";
 
 export const SpendingSummaryListTile = ({
@@ -7,37 +8,28 @@ export const SpendingSummaryListTile = ({
 }: {
   budget: BudgetData;
 }): JSX.Element => {
-  // TODO: refactor duplicated code, see BudgetCard for details as the bellow code is nearly identical
   const { transactions } = useTransactionData();
 
-  const filteredTransactionsByCategory = transactions?.filter((transaction) => {
-    return transaction.category === budget?.category;
-  });
+  const filteredTransactionsByCategory =
+    BudgetViewModel.filterTransactionByBudgetCategory(
+      transactions ?? [],
+      budget
+    );
 
-  filteredTransactionsByCategory?.sort((a, b) => sortByDate(a, b));
-
-  let expendedAmount = 0;
-
-  for (var i = 0; i < filteredTransactionsByCategory!.length; i++) {
-    expendedAmount += filteredTransactionsByCategory?.at(i)?.amount ?? 0;
-  }
-
-  // TODO: refactor duplicated code end
+  const expendedAmount = BudgetViewModel.budgetCategoryExpendedAmount(
+    filteredTransactionsByCategory
+  );
 
   return (
     <div className="spending-summary-list-tile-main">
       {/*  vertical line and label section*/}
       <div className="spending-summary-left-section">
-        {/* list tile vertical line | NOTE: may need to be moved to a css file instead of being styled inline */}
         <div
           style={{
-            width: "4px",
-            height: "100%",
-            borderRadius: "12px",
             backgroundColor: budget.theme,
           }}
         />
-        <p style={{ fontSize: "14px", color: "#696868" }}>{budget.category}</p>
+        <p>{budget.category}</p>
       </div>
 
       <div className="spending-summary-right-section">
