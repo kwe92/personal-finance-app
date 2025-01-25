976a5efb223c1ede1c5fd6afc84ddf68cbe820da
Kwe <Kweayon@gmail.com>
2025-01-25 16:09:44 -0600

    refact fixed auth form responsiveness

--- src/features/auth/signUp/sign_up_view.tsx
+++ src/features/auth/signUp/sign_up_view.tsx
@@ -1,6 +1,4 @@
-import AuthImage from "../shared/components/auth_image";
 import AuthForm from "../shared/components/auth_form";
-import AuthAppBar from "../../shared/components/auth_app_bar";
 
 const SignUpView = (): JSX.Element => {
   return (
