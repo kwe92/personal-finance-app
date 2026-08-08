04860857893dc27bc2f6679fd764bd5e50f00185
Kwe <Kweayon@gmail.com>
2026-08-08 17:26:15 -0500

    removed API call in widget

--- src/features/overview/components/overview_bills_section.tsx
+++ src/features/overview/components/overview_bills_section.tsx
@@ -1,61 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router";
 import "./css/overview_bills_section.css";
 
 import OverviewSectionHeader from "./overview_section_header";
 import { sumOfBills } from "../../shared/utility/functions";
-import { getRecurringBills } from "../../shared/services/backend_service";
 import Skeleton from "../../shared/components/skeleton";
+import { useRecurringBills } from "../../recurringBills/context/recurring_bills_context";
 
 export const OverviewBillsSection = (): JSX.Element => {
   const navigate = useNavigate();
-  const [recurringBills, setRecurringBills] = useState<TransactionData[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  //! TODO: Refactor: this should not be happening within the widget
-  useEffect(() => {
-    let isMounted = true;
-
-    const fetchRecurringBills = async () => {
-      setIsLoading(true);
-
-      try {
-        const response = await getRecurringBills();
-
-        if (!isMounted) {
-          return;
-        }
-
-        setRecurringBills(response.recurringBills ?? []);
-      } catch (error) {
-        if (isMounted) {
-          setRecurringBills([]);
-        }
-      } finally {
-        if (isMounted) {
-          setIsLoading(false);
-        }
-      }
-    };
-
-    fetchRecurringBills();
-
-    return () => {
-      isMounted = false;
-    };
-  }, []);
-
-  const paidBills = recurringBills.filter((bill) => bill.status === "paid");
-  const upcomingBills = recurringBills.filter(
-    (bill) => bill.status === "upcoming",
-  );
-  const dueSoonBills = recurringBills.filter(
-    (bill) => bill.status === "due_soon",
-  );
-
-  const pastDueBills = recurringBills.filter(
-    (bill) => bill.status === "past_due",
-  );
+  const { paidBills, upcomingBills, dueSoonBills, pastDueBills, isLoading } =
+    useRecurringBills();
 
   const sumOfBillsPaid = sumOfBills(paidBills);
   const sumOfBillsUpcoming = sumOfBills(upcomingBills);
